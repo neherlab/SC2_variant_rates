ad5aa3ceedc5f3d0f85a88597e75522235d5bad9
Richard Neher <richard.neher@unibas.ch>
2022-06-25 17:50:12 +0200

    improve_root_to_tip plots

--- Snakefile
+++ Snakefile
@@ -1,9 +1,23 @@
-variants = ['19A', '19B', '20A', '20B', '20C', '20E', '20H', '20I', '20J']
-
+variants = ['19A', '19B', '20A', '20B', '20C', '20E', '20H', '20I', '20J', '21I', '21J', '21K', '21L']
+date_ranges = {
+'19A': (2019.8, 2020.5),
+'19B': (2019.8, 2020.5),
+'20A': (2020.1, 2020.7),
+'20B': (2020.1, 2020.7),
+'20C': (2020.1, 2020.7),
+'20E': (2020.5, 2020.9),
+'20H': (2020.7, 2021.2),
+'20I': (2020.7, 2021.2),
+'20J': (2020.7, 2021.2),
+'21I': (2021.2, 2021.8),
+'21J': (2021.2, 2021.8),
+'21K': (2021.8, 2022.2),
+'21L': (2021.8, 2022.2),
+}
 
 rule get_data:
     output:
-        "metadata.tsv.gz"
+        "data/metadata.tsv.gz"
     shell:
         """
         curl https://data.nextstrain.org/files/ncov/open/metadata.tsv.gz -o {output}
@@ -23,7 +37,7 @@ rule get_clade_data:
 
 rule split_by_variant:
     input:
-        metadata = "metadata.tsv.gz"
+        metadata = "data/metadata.tsv.gz"
     output:
         files = [f"subsets/{v}.tsv" for v in variants]
     params:
@@ -44,3 +58,26 @@ rule clade_genotypes:
         """
         python3 scripts/get_genotypes.py --tree {input.tree} --root {input.root} --output {output.gt}
         """
+
+rule root_to_tip:
+    input:
+        gt = "data/clade_gts.json",
+        metadata = "subsets/{v}.tsv"
+    output:
+        figure = "figures/{v}_rtt.png"
+    params:
+        clade = lambda w: w.v,
+        mindate = lambda w: date_ranges[w.v][0],
+        maxdate = lambda w: date_ranges[w.v][1],
+    shell:
+        """
+        python3 scripts/root_to_tip.py --metadata {input.metadata} --clade {params.clade} \
+                                       --clade-gts data/clade_gts.json \
+                                       --min-date {params.mindate} \
+                                       --max-date {params.maxdate} \
+                                       --output-plot {output.figure}
+        """
+
+rule all_rtt:
+    input:
+        expand("figures/{v}_rtt.png", v=variants)
--- scripts/root_to_tip.py
+++ scripts/root_to_tip.py
@@ -1,8 +1,62 @@
 import pandas as pd
-import argparse
+import argparse,json
 from treetime.utils import numeric_date
 from datetime import datetime
-from scipy.stats import linregress
+import numpy as np
+from scipy.stats import linregress, scoreatpercentile
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+mpl.rcParams['axes.formatter.useoffset'] = False
+
+date_reference = datetime(2020,1,1).toordinal()
+def date_to_week_since2020(d):
+    return (d.toordinal() - date_reference)//7
+
+
+def filter_and_transform(d, clade_gt, min_date=None, max_date=None, completeness=None):
+    # filter for incomplete data
+    d = d.loc[d.date.apply(lambda x:len(x)==10 and 'X' not in x)]
+    d['datetime'] = d.date.apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    d['numdate'] = d.datetime.apply(lambda x: numeric_date(x))
+    d['CW'] = d.datetime.apply(date_to_week_since2020)
+    # filter date range
+    if min_date:
+        d = d.loc[d.numdate>args.min_date]
+    if max_date:
+        d = d.loc[d.numdate<args.max_date]
+
+    # look for clade defining substitutions
+    d["clade_substitutions"] = d.substitutions.apply(lambda x:     [y for y in x.split(',') if y in clade_gt['nuc']] if x else [])
+    # assign number of substitutions missing in the clade definition
+    d["missing_subs"] = d.clade_substitutions.apply(lambda x: len(clade_gt['nuc'])-len(x))
+
+    # define "with-in clade substitutions"
+    d["intra_substitutions"] = d.substitutions.apply(lambda x:     [y for y in x.split(',') if y not in clade_gt['nuc']] if x else [])
+    # make a hashable string representation
+    d["intra_substitutions_str"] = d.intra_substitutions.apply(lambda x: ','.join(x))
+    # define "with-in clade substitutions"
+    d["intra_aaSubstitutions"] = d.aaSubstitutions.apply(lambda x: [y for y in x.split(',') if y not in clade_gt['aa'] and 'ORF9' not in y] if x else [])
+
+    # within clade divergence
+    d["divergence"] =   d.intra_substitutions.apply(lambda x:   len(x))
+    d["aaDivergence"] = d.intra_aaSubstitutions.apply(lambda x: len(x))
+
+    # filter
+    if completeness is not None:
+        return d.loc[d.missing_subs<=completeness]
+
+    return d
+
+def regression_by_week(d, field, min_count=5):
+    val = d.loc[:,[field,'CW']].groupby('CW').mean()
+    std = d.loc[:,[field,'CW']].groupby('CW').std()
+    count = d.loc[:,[field,'CW']].groupby('CW').count()
+    ind = count[field]>min_count
+    reg = linregress(val.index[ind], val.loc[ind, field])
+    slope = reg.slope * 365 / 7
+    intercept = reg.intercept - 2020*slope
+    return {"slope":slope, "intercept":intercept}
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(
@@ -11,14 +65,51 @@ if __name__=="__main__":
     )
 
     parser.add_argument('--metadata', type=str, required=True, help="input data")
+    parser.add_argument('--clade-gts', type=str, required=True, help="input data")
+    parser.add_argument('--clade', type=str, required=True, help="input data")
+    parser.add_argument('--min-date', type=float, help="input data")
+    parser.add_argument('--max-date', type=float, help="input data")
+    parser.add_argument('--output-plot', type=str, help="plot file")
     args = parser.parse_args()
 
+    with open(args.clade_gts) as fh:
+        clade_gt = json.load(fh)[args.clade]
+
     d = pd.read_csv(args.metadata, sep='\t').fillna('')
-    d = d.loc[d.date.apply(lambda x:len(x)==10 and 'X' not in x)]
-    d.loc[:,'numdate'] = d.date.apply(lambda x: numeric_date(datetime.strptime(x, '%Y-%m-%d')))
-    d["divergence"] = d.substitutions.apply(lambda x: len(x.split(',')))
-    d["aaDivergence"] = d.aaSubstitutions.apply(lambda x: len(x.split(',')))
+    filtered_data = filter_and_transform(d, clade_gt, min_date=args.min_date, max_date=args.max_date, completeness=0)
 
-    regression = linregress(d.numdate, d.divergence)
+    regression = linregress(filtered_data.numdate, filtered_data.divergence)
+    filtered_data["residuals"] = filtered_data.apply(lambda x: x.divergence - (regression.intercept + regression.slope*x.numdate), axis=1)
+    iqd = scoreatpercentile(filtered_data.residuals, 75) -  scoreatpercentile(filtered_data.residuals, 25)
+    filtered_data["outlier"] = filtered_data.residuals.apply(lambda x: np.abs(x)>5*iqd)
+    ind = filtered_data.outlier==False
+    # regression_clean = linregress(filtered_data.numdate[ind], filtered_data.divergence[ind])
+    # regression_clean_aa = linregress(filtered_data.numdate[ind], filtered_data.aaDivergence[ind])
+    regression_clean = regression_by_week(filtered_data.loc[ind], "divergence")
+    regression_clean_aa = regression_by_week(filtered_data.loc[ind], "aaDivergence")
 
-    plt.scatter(d.numdate, d.divergence)
+
+    intra_subs_dis = filtered_data["divergence"].value_counts().sort_index()
+    intra_aaSubs_dis = filtered_data["aaDivergence"].value_counts().sort_index()
+    intra_geno = filtered_data["intra_substitutions_str"].value_counts()
+
+    fig, axs = plt.subplots(1,2, figsize=(12,6))
+    ymax = 20
+    sns.histplot(x=filtered_data.numdate, y=np.minimum(ymax*1.5, filtered_data.divergence), bins=(20,ymax+1), ax=axs[0])
+    x = np.array(axs[0].get_xlim())
+    axs[0].plot(x, regression_clean["intercept"] + regression_clean["slope"]*x, lw=4, label=f"slope = {regression_clean['slope']:1.1f} subs/year")
+    axs[0].legend(loc=2)
+    axs[0].set_ylim(0,ymax)
+
+
+    ymax = 20
+    sns.histplot(x=filtered_data.numdate, y=np.minimum(ymax*1.5, filtered_data.aaDivergence), bins=(20,ymax+1), ax=axs[1])
+    x = np.array(axs[0].get_xlim())
+    axs[1].plot(x, regression_clean_aa["intercept"] + regression_clean_aa["slope"]*x, lw=4, label=f"slope = {regression_clean_aa['slope']:1.1f} subs/year")
+    axs[1].legend(loc=2)
+    axs[1].set_ylim(0,ymax)
+
+    if args.output_plot:
+        plt.savefig(args.output_plot)
+    else:
+        plt.show()
